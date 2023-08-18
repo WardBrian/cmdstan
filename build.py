00508ba2ef426bdab3ea285a108192d31b0d0c65
Brian Ward <bward@flatironinstitute.org>
2023-08-18 16:10:24 -0400

    Clean up bin folder

--- build.py
+++ build.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
 from pathlib import Path
 import shutil
 import subprocess
 import sys
 import os
+import argparse
+import platform
 
 # note: since we'd be using system python,
 # we'd want to probably avoid using pathlib
@@ -10,14 +14,16 @@ import os
 
 HERE = Path(__file__).parent
 BUILD = Path(os.getenv("CMDSTAN_BUILD_DIR", HERE/"build"))
+EXE = ".exe" if platform.system() == "Windows" else ""
 
-def main():
+cli = argparse.ArgumentParser()
+cli.add_argument("stan_file", help="stan file to build")
+
+def main(argparsed):
+    args, cmake_args = argparsed
     BUILD.mkdir(exist_ok=True,parents=True)
 
-    if len(sys.argv) < 2:
-        raise ValueError("Must provide a stan file as first argument")
-
-    STAN_FILE = Path(sys.argv[1]).with_suffix(".stan")
+    STAN_FILE = Path(args.stan_file).with_suffix(".stan")
     if not STAN_FILE.exists():
         raise FileNotFoundError(f"Cannot find file '{STAN_FILE}'")
 
@@ -25,20 +31,26 @@ def main():
     with open(HERE/"CMakeLists-model.txt", "w") as f:
         f.write(f"stan_model({STAN_FILE.stem})\n")
     # also need to handle includes
-    shutil.copyfile(STAN_FILE, BUILD/STAN_FILE.name)
+    shutil.copy2(STAN_FILE, BUILD/STAN_FILE.name)
+    # copy executable to build folder to avoid recompilation if we can
+    if (STAN_FILE.with_suffix(EXE).exists()):
+        shutil.copy2(STAN_FILE.with_suffix(EXE), BUILD/"bin"/STAN_FILE.with_suffix(EXE).name)
 
     print("Runing cmake")
-    subprocess.run(["cmake", "-S", str(HERE), "-B", str(BUILD), "-Wno-dev"] + sys.argv[2:], check=True)
+    subprocess.run(["cmake", "-S", str(HERE), "-B", str(BUILD), "-Wno-dev"] + cmake_args, check=True)
 
     print("Building")
     subprocess.run(["cmake", "--build", str(BUILD), '-j', '10', '-t' ,str(STAN_FILE.stem)], check=True)
 
-    shutil.copyfile(BUILD/"bin"/STAN_FILE.stem, STAN_FILE.with_suffix(""))
+    # copy build executable back to original folder, clean up
+    shutil.copy2(BUILD/"bin"/STAN_FILE.with_suffix(EXE).name, STAN_FILE.with_suffix(EXE))
+    (BUILD/"bin"/STAN_FILE.with_suffix(EXE)).unlink(missing_ok=True)
+
     print("Done!")
 
 if __name__ == "__main__":
     try:
-        main()
+        main(cli.parse_known_args())
     except Exception as e:
         print(e)
         sys.exit(1)
